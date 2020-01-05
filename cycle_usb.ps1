4bb2c008177222caf4e2b1f5a0bb0eba4b685e23
Andrew Tonner <rakslice@gmail.com>
2020-01-04 19:14:26 -0800

    tabs to spaces

--- cycle_usb.ps1
+++ cycle_usb.ps1
@@ -1,27 +1,27 @@
 param(
-	[String]$friendlyName,
-	[switch]$uninstall=$false	
+    [String]$friendlyName,
+    [switch]$uninstall=$false   
 )
 
 # A script to cycle the USB controller that a given device is connected to
 
 function SelectWMI ($WMIPathName) {
-	$rest = $WMIPathName.split(':',2)[1]
-	$type = $rest.split('.',2)[0]
-	get-wmiobject $type | where-object -property __PATH -eq $WMIPathName
+    $rest = $WMIPathName.split(':',2)[1]
+    $type = $rest.split('.',2)[0]
+    get-wmiobject $type | where-object -property __PATH -eq $WMIPathName
 }
 
 function ensureSingle($arr) {
-	if ($arr.length -ne 1) {
-		if ($arr.length -gt 1) {
-			write-error "multiple matches:"
-			#write-host $arr
-		} else {
-			write-error "no matches"
-		}
-		exit
-	}
-	$arr[0]
+    if ($arr.length -ne 1) {
+        if ($arr.length -gt 1) {
+            write-error "multiple matches:"
+            #write-host $arr
+        } else {
+            write-error "no matches"
+        }
+        exit
+    }
+    $arr[0]
 }
 
 write-host "Searching for a device with name $friendlyName"
@@ -30,13 +30,13 @@ $usbDevice = ensureSingle(@(get-pnpdevice -presentonly -friendlyname $friendlyNa
 
 write-host $usbDevice.name "is connected to"
 
-$usbDeviceWMI = ensureSingle(@(	get-wmiobject Win32_PnpEntity | 
-	where-object -property PNPDeviceID -eq $usbDevice.instanceid | 
-	where-object -property Present -eq True
+$usbDeviceWMI = ensureSingle(@( get-wmiobject Win32_PnpEntity | 
+    where-object -property PNPDeviceID -eq $usbDevice.instanceid | 
+    where-object -property Present -eq True
 ))
 
 $controllerDevice = ensureSingle(@(get-wmiobject win32_usbcontrollerdevice |
-	where-object -property dependent -eq $usbDeviceWMI.__PATH))
+    where-object -property dependent -eq $usbDeviceWMI.__PATH))
 
 $controller = ensureSingle(@(SelectWMI($controllerDevice.antecedent)))
 $controllerPNP = $controller.PNPDeviceID
@@ -48,9 +48,9 @@ write-host $controllerPNP
 #write-host $controller
 
 if ($uninstall) {
-	$adminScript="uninstall_pnp_device.ps1"
+    $adminScript="uninstall_pnp_device.ps1"
 } else {
-	$adminScript="cycle_pnp_device.ps1"
+    $adminScript="cycle_pnp_device.ps1"
 }
 
 write-host "Launching an admin script to cycle the device"
